0578cb323e91fd47fcbf260c57daca15736aaa7d
Obinna Onyimadu <obinna240@yahoo.co.uk>
2021-07-21 09:14:38 +0100

    test modified

--- src/main/java/com/ob/rover/Rover.java
+++ src/main/java/com/ob/rover/Rover.java
@@ -2,7 +2,74 @@ package com.ob.rover;
 
 public class Rover {
 
+
     public String execute(String commands) {
-        return "0:0:E";
+        String direction = "N";
+
+        for(char com: commands.toCharArray()){
+            direction = detectCommand(com, direction);
+        }
+        String finalDest = "0:0:"+direction;
+        return finalDest;
+    }
+
+    private String detectCommand(char commands, String currentDirection) {
+
+        if (commands == 'R'){
+            return moveRight(currentDirection);
+        }
+        else if (commands == 'L'){
+            return moveLeft(currentDirection);
+        }
+        else {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private String moveRight(String cur_direction){
+        if(cur_direction.equals("N")) {
+            return "E";
+        }
+        else if(cur_direction.equals("E")) {
+            return "S";
+        }
+        else if(cur_direction.equals("S")) {
+            return "W";
+        }
+        else if(cur_direction.equals("W")) {
+            return "N";
+        }
+        else {
+            return "N";
+        }
+    }
+
+    private String moveLeft(String cur_direction){
+        String finalDest = "N";
+
+//        if (month == null) {
+//            return monthNumber;
+//        }
+
+        switch (cur_direction) {
+            case "N":
+                finalDest = "W";
+                break;
+            case "W":
+                finalDest = "S";
+                break;
+            case "S":
+                finalDest = "E";
+                break;
+            case "E":
+                finalDest = "N";
+                break;
+            default:
+                finalDest = "N";
+                break;
+        }
+
+        return finalDest;
     }
 }
--- src/test/java/com/ob/rover/RoverShould.java
+++ src/test/java/com/ob/rover/RoverShould.java
@@ -21,13 +21,31 @@ public class RoverShould {
 
     //So given a starting position (0,0,N) 0, 0, facing North
     //then if i rotate right, then i should face East i.e. (0:0:E)
+    //include an additional parameter to rotate twice
 //    @Test
     @ParameterizedTest
-    @CsvSource()
-    public void roverShouldRotateRight(String command, String position){
+    @CsvSource(delimiter = ',',
+            value = {"R, 0:0:E",
+                    "RR, 0:0:S",
+            "RRR, 0:0:W",
+            "LRLL, 0:0:S"})
+    public void roverShouldRotate(String command, String position){
+//        assertEquals(1,1);
+        assertThat(rover.execute(command)).isEqualTo(position);
+
+    }
+
+    @Test
+    public void roverShouldRotateRight(){
 //        assertEquals(1,1);
         assertThat(rover.execute("R")).isEqualTo("0:0:E");
 
     }
 
+    @Test
+    public void roverShouldRotateRightMore(){
+//        assertEquals(1,1);
+        assertThat(rover.execute("RR")).isEqualTo("0:0:S");
+
+    }
 }
